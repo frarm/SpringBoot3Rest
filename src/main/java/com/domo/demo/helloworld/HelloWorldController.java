eb21d220485a9ff9f3a58a10a0cb5568106f7b7c
frarm <frarm2014@outlook.com>
2022-12-25 16:45:24 -0500

    borrando lo innecasario

--- src/main/java/com/domo/demo/helloworld/HelloWorldController.java
+++ src/main/java/com/domo/demo/helloworld/HelloWorldController.java
@@ -21,12 +21,12 @@ public class HelloWorldController {
     // @RequestMapping(method = (RequestMethod.GET, path = "/hello-world")
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
-        return "Hello World";
+        return "Hello World v2";
     }
 
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
-        return new HelloWorldBean("Hello World");
+        return new HelloWorldBean("Hello World Bean");
     }
 
     @GetMapping(path = "/hello-world/path-variable/{name}")
--- src/main/java/com/domo/demo/jpa/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.domo.demo.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.domo.demo.user.Post;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-
-}
