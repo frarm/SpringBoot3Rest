b868b17a3e752f98f175a9b6ddc5e0c72e2ccf39
frarm <frarm2014@outlook.com>
2022-12-25 14:20:52 -0500

    Security disable

--- src/main/java/com/domo/demo/security/SpringSecurityConfiguration.java
+++ src/main/java/com/domo/demo/security/SpringSecurityConfiguration.java
@@ -1,21 +1,14 @@
 package com.domo.demo.security;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
+// @Configuration
 public class SpringSecurityConfiguration {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(
-                auth -> auth.anyRequest().authenticated());
-        // login web page
-        http.httpBasic(withDefaults());
-        http.csrf().disable();
-        return http.build();
-    }
+    // @Bean
+    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    // http.authorizeRequests(
+    // auth -> auth.anyRequest().authenticated());
+    // // login web page
+    // http.httpBasic(withDefaults());
+    // http.csrf().disable();
+    // return http.build();
+    // }
 }
